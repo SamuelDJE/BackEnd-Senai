08d0064942289a19cfcadfa101c9c0047e2eea3d
SamuelDJE <samuelf.p.d.san@gmail.com>
2026-06-02 10:52:54 -0300

    Add files via upload

---
+++ copa/src/main/java/com/biolab/copa/repositories/SelecaoRepository.java
@@ -0,0 +1,6 @@
+package com.biolab.copa.repositories;
+
+import com.biolab.copa.entities.Selecao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SelecaoRepository extends JpaRepository<Selecao, Long> {}
